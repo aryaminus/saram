11ea3ae3917249145e8a6f19ec18fcd8d6210b7e
Sunim Acharya <sunim.54@gmail.com>
2018-02-10 17:19:13 +0545

    Display cheged files else no directory

--- main.py
+++ main.py
@@ -19,24 +19,23 @@ def check_path(path):
     	return bool(os.path.exists(path)) #Checkif path exists
 
 def main(path):
-	if call(['which', 'tesseract']): #Run the command described by args
+    if call(['which', 'tesseract']): #Run the command described by args
     	print("tesseract-ocr missing") #No tesseract installed
-
     elif check_path(path):
         directory_path = path + '/OCR-text/' #Create text_conversion folder
 
         count = 0
-		other_files = 0
+        other_files = 0
 
         for f in os.listdir(path): #Return list of files in path directory
             ext = os.path.splitext(f)[1] #Split the pathname path into a pair i.e take .png/ .jpg etc
 
             if ext.lower() not in VALIDITY: #Convert to lowercase and check in validity list
-    			other_files += 1 #Increment if other than validity extension found
-				continue
-			else :
+                other_files += 1 #Increment if other than validity extension found
+                continue
+            else :
                 if count == 0: #No directory created
-					create_directory(directory_path) #function to create directory
+                    create_directory(directory_path) #function to create directory
                 count += 1
 
                 image_file_name = path + '/' + f #Full /dir/path/filename.extension
@@ -50,12 +49,15 @@ def main(path):
                 print(str(count) + (" file" if count == 1 else " files") + " processed")
         
         if count + other_files == 0:
-    		print("No files found") #No files found
-
+            print("No files found") #No files found
+        else :
+            print(str(count) + " / " + str(count + other_files) + " files converted")
+    else :
+        print("No directory : " + format(path))
 
 if __name__ == '__main__': #Execute all code before reading source file, ie. execute import, evaluate def to equal name to main
-	if len(sys.argv) != 2: # Count number of arguments which contains the command-line arguments passed to the script if it is not equal to 2 ie for (py main.py 1_arg 2_arg)
-		raise ArgumentMissingException
+    if len(sys.argv) != 2: # Count number of arguments which contains the command-line arguments passed to the script if it is not equal to 2 ie for (py main.py 1_arg 2_arg)
+        raise ArgumentMissingException
     path = sys.argv[1] #python main.py "path_to/img_dir" ie the argv[1] value
     path = os.path.abspath(path) #Accesing filesystem for Return a normalized absolutized version of the pathname path
     main(path) # Def main to path
