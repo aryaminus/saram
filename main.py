15a506b0c64b5f78fcc4e7d5e46346f8c4ad7a6d
Sunim Acharya <sunim.54@gmail.com>
2018-02-10 15:39:37 +0545

    Setup Arg missing exception

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ from subprocess import call
 
 FNULL = open(os.devnull, 'w') #Open file in write mode to The file path of the null device. For example: '/dev/null' 
 
+class ArgumentMissingException(Exception):
+    def __init__(self):
+        print("usage: {} <dirname>".format(sys.argv[0]))
+        sys.exit(1)
+
+if __name__ == '__main__': #Execute all code before reading source file, ie. execute import, evaluate def to equal name to main
+	if len(sys.argv) != 2: # Count number of arguments which contains the command-line arguments passed to the script if it is not equal to 2 ie for (py main.py 1_arg 2_arg)
+		raise ArgumentMissingException
+    
