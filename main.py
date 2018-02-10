6dfbe3807ee69c1268bb9fb6d5b8bf4b1b6c29c0
Sunim Acharya <sunim.54@gmail.com>
2018-02-10 16:36:44 +0545

    Checkif tesseract & path exists

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ class ArgumentMissingException(Exception):
         print("usage: {} <dirname>".format(sys.argv[0]))
         sys.exit(1)
 
+def check_path(path):
+    	return bool(os.path.exists(path)) #Checkif path exists
+
+def main(path):
+	if call(['which', 'tesseract']): #Run the command described by args
+    	print("tesseract-ocr missing")
+    elif check_path(path):
+    
+
 if __name__ == '__main__': #Execute all code before reading source file, ie. execute import, evaluate def to equal name to main
 	if len(sys.argv) != 2: # Count number of arguments which contains the command-line arguments passed to the script if it is not equal to 2 ie for (py main.py 1_arg 2_arg)
 		raise ArgumentMissingException
