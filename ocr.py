9213498688fbbb2c6e4ca4a18831367f344178d7
Sunim Acharya <sunim.54@gmail.com>
2018-02-10 09:47:31 +0545

    Sort and parse args

--- ocr.py
+++ ocr.py
@@ -1,7 +1,16 @@
 # import the necessary packages
-from PIL import Image
-import pytesseract
 import argparse
-import cv2
 import os
- 
+
+import pytesseract
+from PIL import Image
+
+import cv2
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image to be OCR'd")
+ap.add_argument("-p", "--preprocess", type=str, default="thresh",
+	help="type of preprocessing to be done")
+args = vars(ap.parse_args())
