6a215c1ae07f8b88a54c1ac6aecec15712b56718
Sunim Acharya <sunim.54@gmail.com>
2018-02-18 19:12:42 +0545

    Setup for init

--- saram/saram.py
+++ saram/saram.py
@@ -173,3 +173,11 @@ class saram(object):
                 print(str(count) + " / " + str(count + other_files) + " files converted")
         else :
             print("No directory : " + format(path))
+
+def start():
+    if len(sys.argv) != 2: # Count number of arguments which contains the command-line arguments passed to the script if it is not equal to 2 ie for (py main.py 1_arg 2_arg)
+        raise ArgumentMissingException
+    path = sys.argv[1] #python main.py "path_to/img_dir" ie the argv[1] value
+    path = os.path.abspath(path) #Accesing filesystem for Return a normalized absolutized version of the pathname path
+    s = saram()
+    s.main(path) # Def main to path
--- setup.py
+++ setup.py
@@ -6,19 +6,24 @@ def readme():
         return f.read()
 
 setup(
-  name = 'saram',
-  packages = ['saram'], # this must be the same as the name above
-  version = '0.2',
-  description = 'A library to fetch images from a directory and fetch OCR from it with orientation rotation and pdf support',
-  long_description = readme(),
-  author = 'Sunim Acharya',
-  author_email = 'sunim.54@gmail.com',
-  url = 'https://github.com/aryaminus/saram', # use the URL to the github repo
-  keywords = ['ocr', 'image', 'pdf'], # arbitrary keywords
-  classifiers = [],
-  install_requires=[
+    name = 'saram',
+    packages = ['saram'], # this must be the same as the name above
+    version = '0.2',
+    description = 'A library to fetch images from a directory and fetch OCR from it with orientation rotation and pdf support',
+    long_description = readme(),
+    author = 'Sunim Acharya',
+    author_email = 'sunim.54@gmail.com',
+    url = 'https://github.com/aryaminus/saram', # use the URL to the github repo
+    keywords = ['ocr', 'image', 'pdf'], # arbitrary keywords
+    classifiers = [],
+    install_requires=[
         'setuptools', 'Pillow', 'pyocr', 'Wand'
-  ],
-  include_package_data=True,
-  zip_safe=False
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    scripts=['bin/saram'],
+    entry_points={
+    'console_scripts': [
+        'saram = saram.main'
+    ]},
 )
